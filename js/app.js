3b209f1d8f07d899d3e51a4785290b86bcae7e44
Darwin Smith <darwin@Darwins-MacBook-Pro.local>
2018-12-18 17:43:36 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const heroTitle = document.getElementById('title');
+const colors = {
+  initial: "#e42cad",
+  secondary: "rgba(255, 255, 255, 0.5)"
+}
+
+// heroTitle.addEventListener("mouseover", bounce);
+
+// function bounce() {
+//     heroTitle.classList.toggle("bounce");
+// }
