041816b049088ce3fb25fcabdc7a113b1c362a2c
Darwin Smith <darwinpsmith@gmail.com>
2023-12-08 14:12:35 -0500

    chore: lint campaign-data

--- js/campaign-data.js
+++ js/campaign-data.js
@@ -9,230 +9,64 @@
  */
 const campaignData = [
   [
-    {
-      ballType: "americanball",
-      price: 10,
-      owner: "JOE SEIGERT",
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-      owner: "BARBARA TRAVERS",
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-      owner: "MARY PAT SADDLER",
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
-    {
-      ballType: "americanball",
-      price: 10,
-    },
+    { ballType: "americanball", price: 10, owner: "JOE SEIGERT" },
+    { ballType: "americanball", price: 10, owner: "BARBARA TRAVERS" },
+    { ballType: "americanball", price: 10, owner: "MARY PAT SADDLER" },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
+    { ballType: "americanball", price: 10 },
   ],
   [
-    {
-      ballType: "tennis",
-      price: 20,
-      owner: "KEVIN WHITESIDE",
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-      owner: "BARBARA TRAVERS",
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-      owner: "ANDREW HAMMER",
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-      owner: "DREW HALL",
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
-    {
-      ballType: "tennis",
-      price: 20,
-    },
+    { ballType: "tennis", price: 20, owner: "KEVIN WHITESIDE" },
+    { ballType: "tennis", price: 20, owner: "BARBARA TRAVERS" },
+    { ballType: "tennis", price: 20, owner: "ANDREW HAMMER" },
+    { ballType: "tennis", price: 20, owner: "DREW HALL" },
+    { ballType: "tennis", price: 20 },
+    { ballType: "tennis", price: 20 },
+    { ballType: "tennis", price: 20 },
+    { ballType: "tennis", price: 20 },
+    { ballType: "tennis", price: 20 },
+    { ballType: "tennis", price: 20 },
   ],
   [
-    {
-      ballType: "football",
-      price: 30,
-      owner: "BARBARA TRAVERS",
-    },
-    {
-      ballType: "football",
-      price: 30,
-      owner: "BARBARA TRAVERS",
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
-    {
-      ballType: "football",
-      price: 30,
-    },
+    { ballType: "football", price: 30, owner: "BARBARA TRAVERS" },
+    { ballType: "football", price: 30, owner: "BARBARA TRAVERS" },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
+    { ballType: "football", price: 30 },
   ],
   [
-    {
-      ballType: "basketball",
-      price: 40,
-      owner: "KITTY DUFFY",
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-      owner: "BARBARA TRAVERS",
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
-    {
-      ballType: "basketball",
-      price: 40,
-    },
+    { ballType: "basketball", price: 40, owner: "KITTY DUFFY" },
+    { ballType: "basketball", price: 40, owner: "BARBARA TRAVERS" },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
+    { ballType: "basketball", price: 40 },
   ],
   [
-    {
-      ballType: "golf",
-      price: 50,
-      owner: "TOM ECK",
-    },
-    {
-      ballType: "golf",
-      price: 50,
-      owner: "LISA REVORD",
-    },
-    {
-      ballType: "golf",
-      price: 50,
-      owner: "KEVIN CARROL",
-    },
-    {
-      ballType: "golf",
-      price: 50,
-      owner: "MIKE BRANT",
-    },
-    {
-      ballType: "golf",
-      price: 50,
-      owner: "RACHEL DOUGHERTY HENDRICKS",
-    },
-    {
-      ballType: "golf",
-      price: 50,
-    },
-    {
-      ballType: "golf",
-      price: 50,
-    },
-    {
-      ballType: "golf",
-      price: 50,
-    },
-    {
-      ballType: "golf",
-      price: 50,
-    },
-    {
-      ballType: "golf",
-      price: 50,
-    },
+    { ballType: "golf", price: 50, owner: "TOM ECK" },
+    { ballType: "golf", price: 50, owner: "LISA REVORD" },
+    { ballType: "golf", price: 50, owner: "KEVIN CARROL" },
+    { ballType: "golf", price: 50, owner: "MIKE BRANT" },
+    { ballType: "golf", price: 50, owner: "RACHEL DOUGHERTY HENDRICKS" },
+    { ballType: "golf", price: 50 },
+    { ballType: "golf", price: 50 },
+    { ballType: "golf", price: 50 },
+    { ballType: "golf", price: 50 },
+    { ballType: "golf", price: 50 },
   ],
   [
     { ballType: "americanball", price: 60, owner: "BARBARA TRAVERS" },
@@ -295,5 +129,4 @@ const campaignData = [
     { ballType: "golf", price: 100 },
   ],
 ];
-
 window.campaignData = campaignData;
