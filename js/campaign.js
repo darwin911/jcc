2d3783b27a89b02f6638a91f935bb2894f33a0da
Darwin Smith <darwinpsmith@gmail.com>
2023-12-10 11:37:14 -0500

    refactor: break out into separate help functions for readability

--- js/campaign.js
+++ js/campaign.js
@@ -1,48 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const gridContainer = document.querySelector("article.grid-container");
-
+function createGridContainer() {
   const rowContainer = document.createElement("div");
   rowContainer.classList.add("grid");
   rowContainer.id = "grid";
+  return rowContainer;
+}
 
+function createGridItem(item) {
+  const outerWrapperDiv = document.createElement("div");
+  outerWrapperDiv.classList.add("grid-item", item.ballType);
+  outerWrapperDiv.setAttribute("data-price", item.price.toString());
+  return outerWrapperDiv;
+}
+
+function createInnerDollarElement(price) {
+  const innerDollarElement = document.createElement("div");
+  innerDollarElement.innerText = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    trailingZeroDisplay: "stripIfInteger",
+  }).format(price);
+  innerDollarElement.classList.add("item-price");
+  return innerDollarElement;
+}
+
+function enhanceForOwnedItem(outerWrapperDiv, owner) {
+  outerWrapperDiv.classList.add("grayout");
+  outerWrapperDiv.setAttribute("data-name", owner);
+
+  const tooltipContainer = document.createElement("div");
+  tooltipContainer.classList.add("tool");
+
+  const tooltipText = document.createElement("div");
+  tooltipText.innerText = owner;
+  tooltipText.classList.add("toolttext");
+
+  tooltipContainer.appendChild(tooltipText);
+
+  outerWrapperDiv.appendChild(tooltipContainer);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gridContainer = document.querySelector("article.grid-container");
+  const rowContainer = createGridContainer();
   const { campaignData } = window;
 
-  for (let i = 0; i < campaignData.length; i++) {
-    for (let j = 0; j < campaignData[i].length; j++) {
-      const item = campaignData[i][j];
-      const outerWrapperDiv = document.createElement("div");
-      outerWrapperDiv.classList.add("grid-item", item.ballType);
-      outerWrapperDiv.setAttribute("data-price", item.price.toString());
-
-      const innerDollarElement = document.createElement("div");
-      innerDollarElement.innerText = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        trailingZeroDisplay: "stripIfInteger",
-      }).format(item.price);
-      innerDollarElement.classList.add("item-price");
+  campaignData.forEach((row) => {
+    row.forEach((item) => {
+      const outerWrapperDiv = createGridItem(item);
+      const innerDollarElement = createInnerDollarElement(item.price);
 
       outerWrapperDiv.appendChild(innerDollarElement);
 
       if (item.owner) {
-        outerWrapperDiv.classList.add("grayout");
-        outerWrapperDiv.setAttribute("data-name", item.owner);
-
-        const tooltipContainer = document.createElement("div");
-        tooltipContainer.classList.add("tool");
-
-        const tooltipText = document.createElement("div");
-        tooltipText.innerText = item.owner;
-        tooltipText.classList.add("toolttext");
-
-        tooltipContainer.appendChild(tooltipText);
-
-        outerWrapperDiv.appendChild(tooltipContainer);
+        enhanceForOwnedItem(outerWrapperDiv, item.owner);
       }
 
       rowContainer.appendChild(outerWrapperDiv);
-    }
-  }
+    });
+  });
 
   gridContainer.appendChild(rowContainer);
 });
